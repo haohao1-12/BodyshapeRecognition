01fab41e9b0df01e0a69fa3920152f53dc5f3326
haohao1-12 <LiShenghao2020@outlook.com>
2022-05-08 19:20:35 +0100

    Add Images

--- Extraction.py
+++ Extraction.py
@@ -6,3 +6,5 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    #Recolor image
+    frame = cv2.imread('pose1.jpg')
