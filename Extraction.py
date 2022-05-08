0e2c2a4cdccd51452721e856b96bfead61304e4d
haohao1-12 <LiShenghao2020@outlook.com>
2022-05-08 19:14:26 +0100

    add1

---
+++ Extraction.py
@@ -0,0 +1,8 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import matplotlib.pyplot as plt
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
