fac4401881e76bee7c94daa39ad728fd490e014a
haohao1-12 <LiShenghao2020@outlook.com>
2022-05-09 02:43:31 +0100

    test

--- DataBase.py
+++ DataBase.py
@@ -1,4 +1,7 @@
 from Extraction import Extract
 
-example = Extract('pose1.jpg')
-print(example.exe()['lefteye_len'])
+example1 = Extract('pose1.jpg')
+example2 = Extract('side1.jpg')
+#print(example.exe()['lefteye_len'])
+for i in range(len(example1.exe())):
+    selected_features = 
